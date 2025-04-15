644e513349d205664a969dedd7acd9c10290658a
RichardAvery-Uni <20224078@students.latrobe.edu.au>
2025-04-15 23:09:01 +1000

    Base templates for main, report, search, settings, login

--- phase2-build/app/(tabs)/index.tsx
+++ phase2-build/app/(tabs)/index.tsx
@@ -1,23 +1,68 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, ActivityIndicator, Alert, Pressable } from 'react-native';
+import MapView, { Region } from 'react-native-maps';
+import * as Location from 'expo-location';
+import { Ionicons } from '@expo/vector-icons';
+import { router, useRouter } from 'expo-router';
 
 export default function Index() {
-  console.log('Rendering Index component');
+  const [location, setLocation] = useState<Region | undefined>(undefined);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({});
+
+    setLocation({
+      latitude,
+      longitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    });
+
+    setLoading(false);
+  };
+  getCurrentLocation();
+}, []);
+
+  let text = 'Waiting...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.wrapper}>
+      {/*Settings Button*/}
+      <Pressable
+        onPress={() => router.push('/settings')}
+        style={styles.settingsButton}
+      >
+        <Ionicons name="settings" size={24} color="black" />
+      </Pressable>
+
+      {/*Main Title*/}
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Disaster Map</Text>
       </View>
+
+      {/*Map*/}
       <View style={styles.mapContainer}>
         <MapView
           style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
+          initialRegion={location}
+          showsUserLocation={true}
           onMapReady={() => console.log('Map is ready')}
         />
       </View>
@@ -32,12 +77,12 @@ const styles = StyleSheet.create({
   titleContainer: {
     padding: 10, // Adds padding around the title
     alignItems: 'center', // Centers the title horizontally
-    backgroundColor: '#d3d3d3', // Optional: Adds a background color to make the header stand out
+    marginTop: 45,
   },
   title: {
     fontSize: 24, // Larger font size for the title
     fontWeight: 'bold', // Makes the title bold
-    color: '#ffffff', // White color for the title
+    color: 'black', // White color for the title
   },
   mapContainer: {
     flex: 1, // Takes the remaining space after the title
@@ -45,6 +90,13 @@ const styles = StyleSheet.create({
   map: {
     width: '100%',
     height: '100%',
-    marginTop: 0, // 15-unit gap at the top of the map
   },
+  settingsButton: {
+    position: 'absolute',
+    top: 50,
+    left: 20,
+    backgroundColor: '#eee',
+    borderRadius: 10,
+    padding: 10,
+  }
 });
