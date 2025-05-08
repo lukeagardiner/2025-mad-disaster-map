092b5d742972a9449fa3e0cbc9bf92193a44cbea
Lucas G <lukeagardiner@yahoo.com.au>
2025-05-08 19:07:22 +1000

    updating placeholder code

---
+++ phase2-build/app/admin.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Placeholder() {
+  return (
+    <View>
+      <Text>Reset Admin Placeholder</Text>
+    </View>
+  );
+}
