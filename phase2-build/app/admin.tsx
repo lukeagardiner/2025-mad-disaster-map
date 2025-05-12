42f114ff827b9af4ca45ed4aac7497ebbf370200
Lucas G <lukeagardiner@yahoo.com.au>
2025-05-12 22:34:53 +1000

    latest changes cleanups and comments

--- phase2-build/app/admin.tsx
+++ phase2-build/app/admin.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+// broadcast message - in app messaging - firebase
+// extra features for registered users+
+// reporting page - direct edit and global changes -- linked to global messaging -- in app messaging
+// light mode dark mode - maps + screens
+// refactoring - coding style and commenting
+// add apply to be verified user button in settings
+// verified user can add a comment on the event that gets broadcast to subscribers
+
+
 export default function Placeholder() {
   return (
     <View>
