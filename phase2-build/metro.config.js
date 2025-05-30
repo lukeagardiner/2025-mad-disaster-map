3b87d56ed8f7fca369d30de00971f406899c6ce4
Lucas G <lukeagardiner@yahoo.com.au>
2025-05-31 09:11:53 +1000

    adding attempted changes for react raw - rolling back as can't get this working

--- phase2-build/metro.config.js
+++ phase2-build/metro.config.js
@@ -1,31 +1,6 @@
-/*
-const { getDefaultConfig } = require('expo/metro-config'); // existing
-const { withNativeWind } = require('nativewind/metro'); // existing
-
-const config = getDefaultConfig(__dirname); // existing
-
-// new stuff for fixes 1
-config.transformer = {
-    ...config.transformer,
-    //babelTransformerPath: require.resolve('react-native-shadow-gh'),  // fixes replaced
-    babelTransformerPath: require.resolve('react-native-shadow'),
-};
-// new stuff for fixes 2
-config.resolver = {
-    ...config.resolver,
-    assetExts: [...config.resolver.assetExts, 'db', 'ttf', 'obj', 'gltf', 'glb', 'otf',],
-    sourceExts: [...config.resolver.sourceExts, 'cjs'],
-};
-
-
-module.exports = withNativeWind(config, { // existing
-  input: "./app/globals.css", // or wherever your global css is  // existing
-}); // existing 
-*/
-
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './app/globals.css' });
+module.exports = withNativeWind(mergeConfig(config, {}), { input: './app/globals.css' });
