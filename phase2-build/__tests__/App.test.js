1166f5c2f02f68dcac1a7fcc073273cc91655bd7
Lucas G <lukeagardiner@yahoo.com.au>
2025-05-31 16:56:18 +1000

    updating jest test settings

--- phase2-build/__tests__/App.test.js
+++ phase2-build/__tests__/App.test.js
@@ -1,9 +1,17 @@
 import { render } from '@testing-library/react-native';
 //import App from '../App';
 import IndexScreen from '../app/(tabs)/index';
+import { SessionProvider } from '../SessionContext';
+import { ThemeProvider } from '../theme/ThemeContext';
 
 test('renders correctly', () => {
   //const { getByText } = render(<App />);
-  const { getByText } = render(<IndexScreen />);
+  const { getByText } = render(
+    <SessionProvider>
+      <ThemeProvider>
+        <IndexScreen />
+      </ThemeProvider>
+    </SessionProvider>
+  );
   expect(getByText('Open up App.js to start working on your app!')).toBeTruthy();
 });
--- phase2-build/jest.config.js
+++ phase2-build/jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
     preset: 'jest-expo',
-    setupFiles: ['@react-native-async-storage/async-storage/jest/async-storage-mock'],
+    setupFiles: [
+        '../jest.setup.js',
+        '@react-native-async-storage/async-storage/jest/async-storage-mock'
+    ],
     setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
     transformIgnorePatterns: [
-        "node_modules/(?!(jest-)?react-native|@react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)"
+        "node_modules/(?!(jest-)?react-native|@react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|firebase|@firebase)"
     ],
     transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+        "^.+\\.(js|jsx|ts|tsx|mjs)$": "babel-jest"
     },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'mjs'],
 };
