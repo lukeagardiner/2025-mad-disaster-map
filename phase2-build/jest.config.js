21a52d67ade9f5a3d592d6316236ce8594640a8f
Lucas G <lukeagardiner@yahoo.com.au>
2025-05-31 16:34:03 +1000

    updating test for jest settings

--- phase2-build/jest.config.js
+++ phase2-build/jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
     preset: 'jest-expo',
+    setupFiles: ['@react-native-async-storage/async-storage/jest/async-storage-mock'],
     setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
     transformIgnorePatterns: [
-        'node_modules/(?!((jest-)?react-native|@react-native|@expo|expo-.*|@expo-.*/.*|react-native-.*|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base))'
+        "node_modules/(?!(jest-)?react-native|@react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)"
     ],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+    },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 };
