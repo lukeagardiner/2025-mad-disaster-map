60de814b9a0bba76b8e8ba21eeb5fb08e98d2e68
Lucas G <lukeagardiner@yahoo.com.au>
2025-05-31 10:00:41 +1000

    jest config file added

---
+++ phase2-build/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'jest-expo',
+    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+    transformIgnorePatterns: [
+        'node_modules/(?!((jest-)?react-native|@react-native|@expo|expo-.*|@expo-.*/.*|react-native-.*|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base))'
+    ],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+};
